6ca253c7f51f0dbcb24fa30cabda2040c574b373
Tom Burton <tomburton@Raoul-Duke.local>
2011-06-15 17:57:25 +0100

    added job page

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   
   Cufon.replace('#tagline h2' , { fontFamily: 'Gotham Book', hover: true });
-  Cufon.replace('#contact h2, #tagline h2 span, #meet-us h4, #mailing h4, #legal h4, #content h3' , { fontFamily: 'Gotham Medium', hover: true });
+  Cufon.replace('#contact h2, #tagline h2 span, #meet-us h4, #mailing h4, #legal h4, #job h4, #content h3' , { fontFamily: 'Gotham Medium', hover: true });
   
   
 	$('#email').click(
